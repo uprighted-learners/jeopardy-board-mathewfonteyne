19cbeef2b2786498866e22e93ada871f7f31bfcb
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-03-22 00:33:13 -0400

    I still have loads to do. Looking forward to revisiting this project

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,37 @@
 // This makes the "placeholderQuestions" act like a variable essentially.
 import placeholderQuestions from "./placeholder-questions.js";
 // Is an object who's contents are the files data
-console.log({ placeholderQuestions });
+// console.log({ placeholderQuestions });
 // console.log(placeholderQuestions[0]);
 // When I need a question/answer I can iterate over the array
 
+// Window click event, does it work?
+// window.onclick = (e) => {
+//     console.log(e.target); // to get the element
+//     console.log(e.target.tagName); // to get the element tag name alone
+//     console.log(e.target.id); // get element id
+//   };
+
+window.onclick = (e) => {
+    // console.log(e.target); // to get the element
+    // console.log(e.target.tagName); // to get the element tag name alone
+    // console.log(e.target.id); // get element id
+    doTheCardThing(e.target.id);
+};
+let currentCardValue = "";
+function doTheCardThing(id) {
+    document.getElementById(id).innerText =
+    placeholderQuestions[id.slice(-2)]["question"];
+    // console.log(id.slice(-2));
+    currentCard = placeholderQuestions[id.slice(-2)];
+    currentCardValue = document.getElementById(id)[2];
+    // console.log(currentCardValue);
+}
 
 let currentCard = {};
 // * Assign the category name to each grid item
 let categoryOne = document.getElementById("category_name");
-// console.log(categoryOne.textContent);
+// console.log(placeholderQuestions[0]["category"]);
 categoryOne.textContent = placeholderQuestions[0]["category"];
 // console.log(categoryOne.textContent);
 let categoryTwo = document.getElementById("category2_name");
@@ -28,6 +50,7 @@ categoryFive.textContent = placeholderQuestions[40]["category"];
 
 let categorySix = document.getElementById("category6_name");
 categorySix.textContent = placeholderQuestions[50]["category"];
+
 
 // Assign game board questions
 let cat1RdOneFirst = document.getElementById("catone-rd1-200");
@@ -60,6 +83,14 @@ cat1RdOneFifth.addEventListener("click", () => {
     currentCard = placeholderQuestions[4];
 });
 
+let cat1RdTwoFirst = document.getElementById("catone-rd2-400");
+cat1RdTwoFirst.addEventListener("click", () => {
+    cat1RdTwoFirst.innerText = placeholderQuestions[5]["question"];
+    currentCard = placeholderQuestions[5];
+    // console.log(cat1RdTwoFirst);
+    // return answerQuestion()
+});
+
 // let question = document.getElementsByClassName("grid-item");
 // question.addEventListener("click", revealQuestion());
 
@@ -74,24 +105,50 @@ answerForm.addEventListener("submit", (e) => {
     console.log(answer);
     submitAnswer(currentCard, answer)
 });
+
+let answerForm2 = document.getElementById("rd2-form");
+answerForm2.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let answer = answerForm2.answer.value;
+    console.log(answer);
+    submitAnswer2(currentCard, answer)
+});
 function changePlayer() {
     
 }
 
+function submitAnswer2(card, answer) {
+    // onclick event
+    // if (response.toLowerCase() === placeholderQuestions[0]["answer"])
+    console.log("Running submit answer function: ", card, answer);
+    if (answer.toLowerCase() === currentCard.answer.toLowerCase()) {
+        // remove tile from board and add score to current player score
+        alert("great. pick next tile.");
+        // card.removeChild();
+        // let answerBox = document.getElementById("answer-input");
+        answerForm2.reset();
+    } else {
+        // subtract from current player score
+        alert("NO, sorry");
+        answerForm2.reset();
+        // changePlayer();
+    }
+}
 
 function submitAnswer(card, answer) {
     // onclick event
     // if (response.toLowerCase() === placeholderQuestions[0]["answer"])
     console.log("Running submit answer function: ", card, answer);
-    if (answer.toLowerCase() === currentCard.answer) {
+    if (answer.toLowerCase() === currentCard.answer.toLowerCase()) {
         // remove tile from board and add score to current player score
         alert("great. pick next tile.");
-        currentCard.removeChild();
-        let answerBox = document.getElementById("answer-input");
-        answerBox.reset();
+        // card.removeChild();
+        // let answerBox = document.getElementById("answer-input");
+        answerForm.reset();
     } else {
         // subtract from current player score
         alert("NO, sorry");
+        answerForm.reset();
         // changePlayer();
     }
 }
